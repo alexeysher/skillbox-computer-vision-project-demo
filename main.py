a62a6effbaa593ac3d3b468b80a8c22695552e53
alexeysher <asherstobitov@mail.ru>
2025-02-11 12:22:34 +0200

    tf & keras updated

--- main.py
+++ main.py
@@ -333,9 +333,10 @@ def recognize_video_emotions(video_id: VideoId, video_info: VideoInfo, face_dete
         face_image = _extract_face(image, face_detector)
         if face_image is not None:
             faces_number += 1
-            st.write('predicting...')
-            arousal = emotion_recognizer_endpoint.predict(
-                instances=[face_image], use_dedicated_endpoint=True, timeout=5).predictions[0][-1]
+            # st.write('predicting...')
+            # arousal = emotion_recognizer_endpoint.predict(
+            #     instances=[face_image], use_dedicated_endpoint=True, timeout=5).predictions[0][-1]
+            arousal = 0.
         arousals.append(arousal)
         frame_index += 1
         iter_index += 1
